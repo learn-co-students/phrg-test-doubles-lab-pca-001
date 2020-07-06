d57498646085ec75a7c9900c312286ce42baae2c
Aaron Wilson <adub65@gmail.com>
2020-07-06 08:34:25 -0400

    test doubles

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -1,12 +1,11 @@
 require "spec_helper"
 
 RSpec.describe "Doubles" do
-  # Instead of using instances in these tests, 
+  # Instead of using instances in these tests,
   # replace each of these with a 'leaf' test double
   it "leaf returns expected color" do
-    tree = Tree.new
-    branch = Branch.new(tree)
-    leaf = Leaf.new(branch)
+    leaf = double(color: "green")
+
 
     expect(leaf.color).to eq("green")
 
@@ -14,9 +13,7 @@ RSpec.describe "Doubles" do
   end
 
   it "leaf's branch returns expected length" do
-    tree = Tree.new
-    branch = Branch.new(tree)
-    leaf = Leaf.new(branch)
+    leaf = double(branch: double(length: "4 feet"))
 
     expect(leaf.branch.length).to eq("4 feet")
 
@@ -25,9 +22,7 @@ RSpec.describe "Doubles" do
   end
 
   it "leaf's branch's tree returns expected bark" do
-    tree = Tree.new
-    branch = Branch.new(tree)
-    leaf = Leaf.new(branch)
+    leaf = double(branch: (double(tree: double(bark: "thick and brown"))))
 
     expect(leaf.branch.tree.bark).to eq("thick and brown")
 
